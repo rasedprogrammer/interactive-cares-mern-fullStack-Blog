43336c5f1f6044b4e37ad5661fae67286b3920ad
rasedprogrammer <rasedprogrammer@gmail.com>
2025-10-15 23:55:22 +0600

    [UPLOAD] Delelopment Github Repo.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());

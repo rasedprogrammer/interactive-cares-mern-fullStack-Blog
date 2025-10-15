64ed8f8b9858a602424e72f4c0fbd8a8954699f2
rasedprogrammer <rasedprogrammer@gmail.com>
2025-10-15 23:53:03 +0600

    [CREATE] New Project Interactive MERN Full Stack Blog App

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

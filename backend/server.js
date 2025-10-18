7433bedf7d963bb4e0a193b0d84b4f452769f38c
rasedprogrammer <rasedprogrammer@gmail.com>
2025-10-18 11:26:26 +0600

    [CHANGE] the cors origin url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  "origin": ["http://localhost:8000", "http://localhost:5000", "http://localhost:3000", "http://localhost:3001"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

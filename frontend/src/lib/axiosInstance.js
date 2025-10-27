57c83bc06f8802486a8bbc1e075a0d226061de6d
skmasum98 <skmasum98@gmail.com>
2025-10-28 00:26:48 +0600

    delete unwanted

--- frontend/src/lib/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend URL
-  withCredentials: true, // allows cookies/JWT
-});
-
-export default api;
